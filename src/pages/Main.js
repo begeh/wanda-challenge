177225591e731344971cd8ae5acd5e93d17cd5fa
Bashir <b_egeh@hotmail.com>
2020-05-12 15:43:02 -0400

    add hide form feature to register user btn

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -10,13 +10,14 @@ export default function Main(props){
   const [name, setName] = useState(null);
   const [url, setUrl] = useState(null); 
   const [list, setList] = useState(experts);
+  const [show, setShow] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     if(!name || !url){
       return;
     }
-    
+
     const newUser ={
       "id": list.length,
       "name": name,
@@ -30,6 +31,7 @@ export default function Main(props){
     setUrl("");
 
   }
+  
   return(
     <div className="App">
       <h1>Expert Search</h1>
@@ -37,14 +39,26 @@ export default function Main(props){
           Find an expert
         </p>
         <input id="search" className="input" type="textarea" />
-        <Button className= "new-user-button" variant="secondary" size="lg">
+        <Button onClick={e => {
+          if(show){
+            setShow(false)
+          } else{
+            setShow(true);
+          }
+        }} className= "new-user-button" variant="secondary" size="lg">
           Register New User
         </Button>
-        <form className="add-user" onSubmit={(event)=> handleSubmit(event)}>
+
+        {
+          show ?  
+          <form className="add-user" onSubmit={(event)=> handleSubmit(event)}>
           <input placeholder="Enter Name" className="input" type="textarea" value={name} onChange={e => setName(e.target.value)} />
           <input placeholder="Enter Website URL" className="input" type="textarea" onChange={e => setUrl(e.target.value)} value={url}/>
           <input type="submit" className= "btn btn-primary register" value="Submit" />
         </form>
+        : null
+        }
+        
         <Table className="table" striped bordered hover variant="dark">
           <thead>
             <tr>
