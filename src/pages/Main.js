cc27ccc47f782a80f74fc95189625f543300bc26
Bashir <b_egeh@hotmail.com>
2020-05-12 15:33:25 -0400

    add user registration that appends new user to table

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import { Table, Button } from 'react-bootstrap';
 
@@ -6,17 +6,45 @@ import experts from "../experts.json";
 
 import TableItem from "../components/TableItem";
 
-export default function Main(){
+export default function Main(props){
+  const [name, setName] = useState(null);
+  const [url, setUrl] = useState(null); 
+  const [list, setList] = useState(experts);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if(!name || !url){
+      return;
+    }
+    
+    const newUser ={
+      "id": list.length,
+      "name": name,
+      "longUrl": url,
+      "shortUrl": null,
+      "friends": [],
+      "headings": []
+    }
+    setList([...list, newUser]);
+    setName("");
+    setUrl("");
+
+  }
   return(
     <div className="App">
       <h1>Expert Search</h1>
         <p>
           Find an expert
         </p>
-        <input className="search" type="textarea" />
+        <input id="search" className="input" type="textarea" />
         <Button className= "new-user-button" variant="secondary" size="lg">
           Register New User
         </Button>
+        <form className="add-user" onSubmit={(event)=> handleSubmit(event)}>
+          <input placeholder="Enter Name" className="input" type="textarea" value={name} onChange={e => setName(e.target.value)} />
+          <input placeholder="Enter Website URL" className="input" type="textarea" onChange={e => setUrl(e.target.value)} value={url}/>
+          <input type="submit" className= "btn btn-primary register" value="Submit" />
+        </form>
         <Table className="table" striped bordered hover variant="dark">
           <thead>
             <tr>
@@ -29,8 +57,9 @@ export default function Main(){
           </thead>
           <tbody>
               {
-                experts.map(expert => (
+                list.map((expert, index) => (
                   <TableItem 
+                    key={index}
                     name={expert.name}
                     shortUrl={expert.shortUrl}
                     friends={expert.friends}
